3bf43c37bfb87fffcb837851d53161494ef81c7a
NotSnowWhite <gflatch@att.net>
2024-04-25 15:18:04 -0500

    updated prompts in index.js. added data to shapes.js. added date.now method to index.js to populate a new number per file

--- index.js
+++ index.js
@@ -2,30 +2,50 @@ const fs = require('fs');
 const shapes = require('./lib/shapes');
 const inquirer = require('inquirer');
 
-inquirer.prompt([
-    {
-        name: 'text',
-        type: 'input',
-        message: 'Write up to 3 characters to be displayed inside the logo:'
-    },
+function writeToFile(filename, response) {
+    fs.writeFile(`examples/${filename}.svg`, response, (err) => {
+        err ? console.error(err) : console.log('Your logo was successfully created! Check it out.');
+    })
 
-    {
-        name: 'text color',
-        type: 'input',
-        message: 'Input a text color for your logo:'
-    },
+}
 
-    {
-        name: 'shape',
-        type: 'list',
-        message: 'Pick a shape for your logo:',
-        choices: ['Square', 'Circle', 'Triangle']
-    },
+// prompts user for data
+function init() {
+    inquirer.prompt([
+        {
+            name: 'text',
+            type: 'input',
+            message: 'Write up to 3 characters to be displayed inside the logo:'
+        },
 
-    {
-        name: 'shape color',
-        type: 'input',
-        message: 'Input a color for your logo:'
-    },
+        {
+            name: 'textColor',
+            type: 'input',
+            message: 'Input a text color for your logo:'
+        },
 
-])
+        {
+            name: 'shape',
+            type: 'list',
+            message: 'Pick a shape for your logo:',
+            choices: ['Square', 'Circle', 'Triangle']
+        },
+
+        {
+            name: 'shapeColor',
+            type: 'input',
+            message: 'Input a color for your logo:'
+        },
+        {
+            name: 'borderColor',
+            type: 'input',
+            message: 'Input a color for your logo\'s border:'
+        },
+
+    ])
+    .then((response) => {
+        writeToFile(`logo_${Date.now()}`, shapes(response));
+    });
+
+}
+init()
